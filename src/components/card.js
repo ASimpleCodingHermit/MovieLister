98836eafe5bf6dd200fccf6a79f29076e5ed4118
Joseph Chu <hotcupofjoe2013@gmail.com>
2020-08-14 18:50:18 -0400

    Added components and containers, as well as added React skeleton and packages

---
+++ src/components/card.js
@@ -0,0 +1,6 @@
+import React from 'react';
+const Card = () => {
+  return <h2>Movie #1</h2>;
+};
+
+export default Card;
---
+++ src/containers/list.js
@@ -0,0 +1,12 @@
+// Imports
+import React, { Component } from 'react';
+import Card from '../components/card';
+
+// class List
+class List extends Component {
+  render() {
+    return <Card />;
+  }
+}
+
+export default List;
